073532d4d32f8bcc3e141f66e0c4022d83c4fe12
Matvey Kostukovsky <mkostukovsky@intouchconnections.com>
2021-02-19 20:26:15 -0600

    simplify the code

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,27 +1,23 @@
-var readline = require('readline');
-var rl = readline.createInterface({
+let readline = require('readline');
+let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
 });
-
-var lineIndex = 0
 
 function isValueEven(value) {
     return parseInt(value) % 2 == 0
 }
 
 rl.on('line', function (line) {
-    if (lineIndex == 0) {
-        let values = line.split(" ");
-        var allValuesHaveSameParity = isValueEven(values[0]) == isValueEven(values[1]) && isValueEven(values[0]) == isValueEven(values[2])
-        if (allValuesHaveSameParity) {
-            rl.close()
-            console.log("WIN")
-        } else {
-            rl.close()
-            console.log("FAIL")
-        }
+
+    let values = line.split(" ");
+    let allValuesHaveSameParity = isValueEven(values[0]) == isValueEven(values[1])
+        && isValueEven(values[0]) == isValueEven(values[2])
+    if (allValuesHaveSameParity) {
+        console.log("WIN")
+    } else {
+        console.log("FAIL")
     }
 })
 
